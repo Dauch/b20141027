e0541422027966ba7e04d4042093703b5b852c71
lichaoxin <lichaoxin168@gmail.com>
2014-11-06 17:03:59 +0800

    Revert "add easy code to use lua"
    This reverts commit 441869f5cdbbdf5172e1f2fa9da2964dd14d8719.

--- lua_cocos_helper/easyCode.lua
+++
@@ -1,9 +0,0 @@
--------------------------------------
---  easy multi function call
--------------------------------------
-local function func1()
-end
-local function func2()
-end
-action={"1":func1,"2":func2}
-action["1"]()

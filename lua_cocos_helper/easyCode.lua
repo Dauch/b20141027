441869f5cdbbdf5172e1f2fa9da2964dd14d8719
lichaoxin <lichaoxin168@gmail.com>
2014-11-06 16:58:40 +0800

    add easy code to use lua

---
+++ lua_cocos_helper/easyCode.lua
@@ -0,0 +1,9 @@
+-------------------------------------
+--  easy multi function call
+-------------------------------------
+local function func1()
+end
+local function func2()
+end
+action={"1":func1,"2":func2}
+action["1"]()
